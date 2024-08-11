0e3fa3ebe3d5dfbb9e770377541854951de05d93
ohchase <ohchase@proton.me>
2024-08-11 08:20:13 -0400

    promote try find function to api

--- src/lib.rs
+++ src/lib.rs
@@ -450,6 +450,87 @@ impl<'a> DynamicLibrary<'a> {
         })
     }
 
+    /// Finding target function differs on 32 bit and 64 bit.
+    /// On 32 bit we want to check the relocations table only, opposed to the addend relocations table.
+    /// Additionally, we will fall back to the plt given it is an addendless relocation table.
+    #[cfg(target_pointer_width = "32")]
+    pub fn try_find_function(&self, symbol_name: &str) -> Option<&'_ elf32::DynRel> {
+        let string_table = self.string_table();
+        let dyn_symbols = self.symbols()?;
+        if let Some(dyn_relas) = self.relocs() {
+            let dyn_relas = dyn_relas.entries().iter();
+            if let Some(symbol) = dyn_relas
+                .flat_map(|e| {
+                    dyn_symbols
+                        .resolve_name(e.symbol_index() as usize, string_table)
+                        .map(|s| (e, s))
+                })
+                .filter(|(_, s)| s.eq(symbol_name))
+                .next()
+                .map(|(target_function, _)| target_function)
+            {
+                return Some(symbol);
+            }
+        }
+
+        if let Some(dyn_relas) = self.plt_rel() {
+            let dyn_relas = dyn_relas.entries().iter();
+            if let Some(symbol) = dyn_relas
+                .flat_map(|e| {
+                    dyn_symbols
+                        .resolve_name(e.symbol_index() as usize, string_table)
+                        .map(|s| (e, s))
+                })
+                .filter(|(_, s)| s.eq(symbol_name))
+                .next()
+                .map(|(target_function, _)| target_function)
+            {
+                return Some(symbol);
+            }
+        }
+        return None;
+    }
+
+    /// Finding target function differs on 32 bit and 64 bit.
+    /// On 64 bit we want to check the addended relocations table only, opposed to the addendless relocations table.
+    /// Additionally, we will fall back to the plt given it is an addended relocation table.
+    #[cfg(target_pointer_width = "64")]
+    pub fn try_find_function(&self, symbol_name: &str) -> Option<&'_ elf64::DynRela> {
+        let string_table = self.string_table();
+        let symbols = self.symbols()?;
+        if let Some(dyn_relas) = self.addend_relocs() {
+            let dyn_relas = dyn_relas.entries().iter();
+            if let Some(symbol) = dyn_relas
+                .flat_map(|e| {
+                    symbols
+                        .resolve_name(e.symbol_index() as usize, string_table)
+                        .map(|s| (e, s))
+                })
+                .filter(|(_, s)| s.eq(symbol_name))
+                .next()
+                .map(|(target_function, _)| target_function)
+            {
+                return Some(symbol);
+            }
+        }
+
+        if let Some(dyn_relas) = self.plt_rela() {
+            let dyn_relas = dyn_relas.entries().iter();
+            if let Some(symbol) = dyn_relas
+                .flat_map(|e| {
+                    symbols
+                        .resolve_name(e.symbol_index() as usize, string_table)
+                        .map(|s| (e, s))
+                })
+                .filter(|(_, s)| s.eq(symbol_name))
+                .next()
+                .map(|(target_function, _)| target_function)
+            {
+                return Some(symbol);
+            }
+        }
+        return None;
+    }
     /// Access the plt as a dynamic relocation table if possible
     /// can fail if the plt is not available or the plt is with addend
     pub fn plt_rel(&self) -> Option<&DynamicRelocations<'_>> {
