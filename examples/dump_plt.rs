e3ceebb12d950f70fce6568a987d1d797c596be5
ohchase <ohchase@proton.me>
2025-03-08 09:35:10 -0500

    v0.3.1

--- examples/dump_plt.rs
+++ examples/dump_plt.rs
@@ -3,66 +3,66 @@ use plt_rs::{collect_modules, DynamicLibrary, RelocationTable};
 
 fn main() -> Result<()> {
     let entries = collect_modules();
-    println!("collected modules");
+    println!("collected {} modules", entries.len());
 
     for entry in entries.into_iter() {
-        println!("[{:?}] Addr: {:#X?}", entry.name(), entry.addr());
-        if let Ok(dynamic_lib) = DynamicLibrary::initialize(entry) {
+        let entry_name = entry.name().to_owned();
+        println!("[{}] Addr: {:#X?}", entry_name, entry.addr());
+
+        let Ok(dynamic_lib) = DynamicLibrary::initialize(entry) else {
             println!(
-                "Dynamic String Table Length: {}",
-                dynamic_lib.string_table().total_size()
+                "failed to parse {} as dynamic library, skipping...",
+                entry_name
             );
+            continue;
+        };
 
-            let dynamic_symbols = dynamic_lib.symbols().expect("symbols...");
-            let string_table = dynamic_lib.string_table();
+        let Some(dynamic_symbols) = dynamic_lib.symbols() else {
+            println!("failed to retrieve dynamic symbols, skipping...");
+            continue;
+        };
+        let string_table = dynamic_lib.string_table();
 
-            println!("dynamic addend relocations:");
-            if let Some(dyn_relas) = dynamic_lib.addend_relocs() {
-                dyn_relas
-                    .entries()
-                    .iter()
-                    .flat_map(|e| {
-                        dynamic_symbols.resolve_name(e.symbol_index() as usize, string_table)
-                    })
-                    .filter(|s| !s.is_empty())
-                    .for_each(|s| println!("\t{}", s));
-            }
+        println!("dynamic addend relocations:");
+        if let Some(dyn_relas) = dynamic_lib.addend_relocs() {
+            dyn_relas
+                .entries()
+                .iter()
+                .flat_map(|e| dynamic_symbols.resolve_name(e.symbol_index() as usize, string_table))
+                .filter(|s| !s.is_empty())
+                .for_each(|s| println!("\t{}", s));
+        }
 
-            println!("dynamic relocations:");
-            if let Some(dyn_relocs) = dynamic_lib.relocs() {
-                dyn_relocs
-                    .entries()
-                    .iter()
-                    .flat_map(|e| {
-                        dynamic_symbols.resolve_name(e.symbol_index() as usize, string_table)
-                    })
-                    .filter(|s| !s.is_empty())
-                    .for_each(|s| println!("\t{}", s));
-            }
+        println!("dynamic relocations:");
+        if let Some(dyn_relocs) = dynamic_lib.relocs() {
+            dyn_relocs
+                .entries()
+                .iter()
+                .flat_map(|e| dynamic_symbols.resolve_name(e.symbol_index() as usize, string_table))
+                .filter(|s| !s.is_empty())
+                .for_each(|s| println!("\t{}", s));
+        }
 
-            println!("plt:");
-            if let Some(plt) = dynamic_lib.plt() {
-                match plt {
-                    RelocationTable::WithAddend(rel) => {
-                        rel.entries()
-                            .iter()
-                            .flat_map(|e| {
-                                dynamic_symbols
-                                    .resolve_name(e.symbol_index() as usize, string_table)
-                            })
-                            .filter(|s| !s.is_empty())
-                            .for_each(|s| println!("\t{}", s));
-                    }
-                    RelocationTable::WithoutAddend(rel) => {
-                        rel.entries()
-                            .iter()
-                            .flat_map(|e| {
-                                dynamic_symbols
-                                    .resolve_name(e.symbol_index() as usize, string_table)
-                            })
-                            .filter(|s| !s.is_empty())
-                            .for_each(|s| println!("\t{}", s));
-                    }
+        println!("plt:");
+        if let Some(plt) = dynamic_lib.plt() {
+            match plt {
+                RelocationTable::WithAddend(rel) => {
+                    rel.entries()
+                        .iter()
+                        .flat_map(|e| {
+                            dynamic_symbols.resolve_name(e.symbol_index() as usize, string_table)
+                        })
+                        .filter(|s| !s.is_empty())
+                        .for_each(|s| println!("\t{}", s));
+                }
+                RelocationTable::WithoutAddend(rel) => {
+                    rel.entries()
+                        .iter()
+                        .flat_map(|e| {
+                            dynamic_symbols.resolve_name(e.symbol_index() as usize, string_table)
+                        })
+                        .filter(|s| !s.is_empty())
+                        .for_each(|s| println!("\t{}", s));
                 }
             }
         }
